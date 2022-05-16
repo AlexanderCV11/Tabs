88c0d4b329d331112c0998b5f11ed14aab199112
alexan <alexander_corral_valdez@outlook.com>
2022-05-16 13:12:46 -0500

    first commit

---
+++ App.js
@@ -0,0 +1,20 @@
+import {NavigationContainer} from '@react-navigation/native';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import NewsScreen from './NewsScreen';
+import SettingsScreen from './SettingsScreen';
+import HomeScreen from './HomeScreen';
+
+const Tab = createBottomTabNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name= "Home" component={HomeScreen}/>
+        <Tab.Screen name= "News" component={NewsScreen}/>
+        <Tab.Screen name= "Settings" component={SettingsScreen}/>
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+}
+
